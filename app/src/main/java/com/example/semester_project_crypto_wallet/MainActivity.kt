93edaf90cb4e8d820305304efb2a90db2b39efae
Patrik Majtán <xmajtanp@stuba.sk>
2021-11-18 21:29:03 +0100

    feat: added stellar and Databinding to project

--- app/src/main/java/com/example/semester_project_crypto_wallet/MainActivity.kt
+++ app/src/main/java/com/example/semester_project_crypto_wallet/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.semester_project_crypto_wallet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.semester_project_crypto_wallet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+//    var pair: KeyPair = KeyPair.random()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.button1.setOnClickListener{
+            editNickname("Button1")
+
+        }
+    }
+
+    private fun editNickname(s: String) {
+        binding.button1.text = s
     }
 }
