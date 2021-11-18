69f1ad921b081b991302f55e992f324cbbc69e0b
Patrik Majtán <xmajtanp@stuba.sk>
2021-11-18 22:40:34 +0100

    fix: stellar in project is not from local file

--- app/src/main/java/com/example/semester_project_crypto_wallet/MainActivity.kt
+++ app/src/main/java/com/example/semester_project_crypto_wallet/MainActivity.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.semester_project_crypto_wallet.databinding.ActivityMainBinding
+import org.stellar.sdk.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 //    var pair: KeyPair = KeyPair.random()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+
+        setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         binding.button1.setOnClickListener{
             editNickname("Button1")
