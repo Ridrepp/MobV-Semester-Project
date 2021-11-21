9514c257e5396e28b2a580a090decca9c4335932
kalivodakristian <kalivoda.kristian@gmail.com>
2021-11-21 17:18:02 +0100

    feat: add repository concept

---
+++ app/src/main/java/com/example/semester_project_crypto_wallet/Repository.kt
@@ -0,0 +1,36 @@
+package com.example.semester_project_crypto_wallet
+
+import androidx.annotation.WorkerThread
+import com.example.semester_project_crypto_wallet.data.DbDao
+import com.example.semester_project_crypto_wallet.data.entities.Receiver
+import com.example.semester_project_crypto_wallet.data.entities.Transaction
+
+class Repository(private val dbDao: DbDao) {
+    // Room executes all queries on a separate thread.
+    // Observed Flow will notify the observer when the data has changed.
+//    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insertTransaction(transaction: Transaction) {
+        dbDao.insertTransaction(transaction)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun insertReceiver(receiver: Receiver) {
+        dbDao.insertReceiver(receiver)
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun getTransactions() {
+        dbDao.getTransactions()
+    }
+
+    @Suppress("RedundantSuspendModifier")
+    @WorkerThread
+    suspend fun getReceivers() {
+        dbDao.getReceivers()
+    }
+}
