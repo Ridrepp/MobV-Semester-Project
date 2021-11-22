1e5d600f4473f6bedb327fda66be7f9c1b990c1a
Patrik Majtán <xmajtanp@stuba.sk>
2021-11-22 21:47:41 +0100

    feat: code commenting due to malfunction through modifications, design modification (test), fragment switching using the button (test) - need editing

--- app/src/main/java/com/example/semester_project_crypto_wallet/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/semester_project_crypto_wallet/ui/home/HomeFragment.kt
@@ -1,18 +1,15 @@
 package com.example.semester_project_crypto_wallet.ui.home
 
 import android.os.Bundle
-import android.os.StrictMode
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import com.example.semester_project_crypto_wallet.R
-import com.example.semester_project_crypto_wallet.User
 import com.example.semester_project_crypto_wallet.databinding.FragmentHomeBinding
-import org.stellar.sdk.KeyPair
 
 class HomeFragment : Fragment() {
     private val homeViewModel: HomeViewModel by viewModels()
@@ -26,40 +23,41 @@ class HomeFragment : Fragment() {
 
         //return inflater.inflate(R.layout.fragment_home, container, false)
 
+
+
+
         // Changing policy because of response from Stellar server
-        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy)
-
-
-        binding.createAccount.setOnClickListener{
-            val pair = KeyPair.random()
-            editTextViews(pair)
-        }
-
-
-        binding.button.setOnClickListener{
-            // First user
-            val first_user = User()
-            first_user.createAccount()
-            first_user.checkBalances()
-
-            //Second user
-            val second_user = User()
-            second_user.createAccount()
-            second_user.checkBalances()
-
-            Log.i("****************","********TRANSACTION*************")
-            first_user.sendPayment(second_user.my_keypair)
-
-            Log.i("****************","********BALANCES AGAIN*************")
-            first_user.checkBalances()
-            second_user.checkBalances()
-
-            Log.i("****************","********END*************")
-
-
-
-        }
+//        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+//        StrictMode.setThreadPolicy(policy)
+//        binding.createAccount.setOnClickListener{
+//            val pair = KeyPair.random()
+//            editTextViews(pair)
+//        }
+//
+//
+//        binding.button.setOnClickListener{
+//            // First user
+//            val first_user = User()
+//            first_user.createAccount()
+//            first_user.checkBalances()
+//
+//            //Second user
+//            val second_user = User()
+//            second_user.createAccount()
+//            second_user.checkBalances()
+//
+//            Log.i("****************","********TRANSACTION*************")
+//            first_user.sendPayment(second_user.my_keypair)
+//
+//            Log.i("****************","********BALANCES AGAIN*************")
+//            first_user.checkBalances()
+//            second_user.checkBalances()
+//
+//            Log.i("****************","********END*************")
+//
+//
+//
+//        }
 
         binding.transactionsButton.setOnClickListener{
 
@@ -67,17 +65,20 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-
-    private fun editTextViews(pair: KeyPair) {
-        binding.apply {
-            publicKey.text = pair.accountId
-            privateKey.text = String(pair.secretSeed)
-
-        }
+        binding.paymentButton.setOnClickListener { it.findNavController().navigate(R.id.action_homeFragment_to_paymentFragment) }
     }
+//    private fun editTextViews(pair: KeyPair) {
+//        binding.apply {
+//            publicKey.text = pair.accountId
+//            privateKey.text = String(pair.secretSeed)
+//
+//        }
+//    }
 
-    private fun createAccount(user: User){
-        user.createAccount()
-    }
+//    private fun createAccount(user: User){
+//        user.createAccount()
+//    }
 }
--- app/src/main/java/com/example/semester_project_crypto_wallet/ui/payments/PaymentFragment.kt
+++ app/src/main/java/com/example/semester_project_crypto_wallet/ui/payments/PaymentFragment.kt
@@ -1,13 +1,13 @@
 package com.example.semester_project_crypto_wallet.ui.payments
 
 import android.os.Bundle
-import android.os.StrictMode
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
 import com.example.semester_project_crypto_wallet.R
 import com.example.semester_project_crypto_wallet.databinding.FragmentPaymentBinding
 
@@ -22,5 +22,10 @@ class PaymentFragment : Fragment(){
         binding.lifecycleOwner = viewLifecycleOwner
 
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.contactsButton.setOnClickListener { it.findNavController().navigate(R.id.action_paymentFragment_to_contactsFragment) }
     }
 }
