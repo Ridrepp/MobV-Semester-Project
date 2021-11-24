c80564bd04db8a0cc823da4dd532b885c1c464e9
kalivodakristian <kalivoda.kristian@gmail.com>
2021-11-24 19:54:41 +0100

    fix: pin confirmation

--- app/src/main/java/com/example/semester_project_crypto_wallet/ui/pin/PinFragment.kt
+++ app/src/main/java/com/example/semester_project_crypto_wallet/ui/pin/PinFragment.kt
@@ -1,14 +1,17 @@
 package com.example.semester_project_crypto_wallet.ui.pin
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.semester_project_crypto_wallet.R
 import com.example.semester_project_crypto_wallet.databinding.FragmentPinBinding
+import kotlinx.coroutines.launch
 
 class PinFragment : Fragment(){
     private val pinViewModel: PinViewModel by viewModels()
@@ -20,10 +23,21 @@ class PinFragment : Fragment(){
         binding.pinModel = pinViewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
+        //TODO: check if it is supposed to be onCreateView or onViewCreated
+        binding.button2.setOnClickListener{
+            lifecycleScope.launch {
+                pinViewModel.confirmPin()
+            }
+        }
+
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+//        pinViewModel.pin.observe(viewLifecycleOwner){
+//            Log.i("pin","$it")
+//        }
 
     }
 }
--- app/src/main/java/com/example/semester_project_crypto_wallet/ui/pin/PinViewModel.kt
+++ app/src/main/java/com/example/semester_project_crypto_wallet/ui/pin/PinViewModel.kt
@@ -1,13 +1,22 @@
 package com.example.semester_project_crypto_wallet.ui.pin
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class PinViewModel: ViewModel() {
-    private val _pin: MutableLiveData<String> = MutableLiveData()
+    val pin: MutableLiveData<String> = MutableLiveData()
 
-    val pin: LiveData<String>
-        get() = _pin
+//    val pin: LiveData<String>
+//        get() = _pin
 
+    fun confirmPin(){
+        pin.value?.let {
+            if(it.isNotEmpty()){
+                //TODO: process pin
+                Log.i("confirm_pin", it)
+            }
+        }
+    }
 }
