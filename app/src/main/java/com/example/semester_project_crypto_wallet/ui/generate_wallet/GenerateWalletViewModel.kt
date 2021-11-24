69838270255b614b7875d90df4c1b775cb130cdd
kalivodakristian <kalivoda.kristian@gmail.com>
2021-11-24 19:55:22 +0100

    TODO: GenerateWallet

--- app/src/main/java/com/example/semester_project_crypto_wallet/ui/generate_wallet/GenerateWalletViewModel.kt
+++ app/src/main/java/com/example/semester_project_crypto_wallet/ui/generate_wallet/GenerateWalletViewModel.kt
@@ -3,4 +3,9 @@ package com.example.semester_project_crypto_wallet.ui.generate_wallet
 import androidx.lifecycle.ViewModel
 
 class GenerateWalletViewModel : ViewModel() {
+    //TODO:
+    //  textview public key
+    //  textview private key
+    //  editview pin kod
+    //  button potvrdit pin
 }
