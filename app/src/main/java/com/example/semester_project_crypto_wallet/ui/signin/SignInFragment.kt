68bba0a21e60756cf48f08c1b2baf70072a7d59a
kalivodakristian <kalivoda.kristian@gmail.com>
2021-11-27 20:58:06 +0100

    Feat: sign in

--- app/src/main/java/com/example/semester_project_crypto_wallet/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/example/semester_project_crypto_wallet/ui/signin/SignInFragment.kt
@@ -1,18 +1,23 @@
 package com.example.semester_project_crypto_wallet.ui.signin
 
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.semester_project_crypto_wallet.R
 import com.example.semester_project_crypto_wallet.data.util.Injection
 import com.example.semester_project_crypto_wallet.databinding.FragmentSigninBinding
+import kotlinx.coroutines.launch
 
 class SignInFragment : Fragment() {
     private lateinit var signInViewModel: SignInViewModel
@@ -37,29 +42,42 @@ class SignInFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.signInButton.setOnClickListener {
-            if (signInViewModel.logInAccount()) {
-                signInViewModel.findKeyCredential.observe(
-                    viewLifecycleOwner,
-                    {
-                        if (it == null){
-                            Toast.makeText(context, "Konto so zadaným public key nebolo nájdené.", Toast.LENGTH_LONG).show()
-                        }
-                        else {
-                            Log.i("FOUNDED PUBLIC", it.publicKey)
-                            Log.i("FOUNDED PRIVATE", it.privateKey)
+//            if (signInViewModel.logInAccount()) {
+//                signInViewModel.findKeyCredential.observe(
+//                    viewLifecycleOwner,
+//                    {
+//                        if (it == null){
+//                            Toast.makeText(context, "Konto so zadaným public key nebolo nájdené.", Toast.LENGTH_LONG).show()
+//                        }
+//                        else {
+//                            Log.i("FOUNDED PUBLIC", it.publicKey)
+//                            Log.i("FOUNDED PRIVATE", it.privateKey)
+//
+//                            findNavController().navigate(R.id.action_signInFragment_to_loggedInFragment)
+//                        }
+//                    })
+//            } else
+//                Toast.makeText(
+//                    context, "Nebol zadaný public key.",
+//                    Toast.LENGTH_SHORT
+//                ).show()
 
-                            findNavController().navigate(R.id.action_signInFragment_to_loggedInFragment)
-                        }
-                    })
-            } else
-                Toast.makeText(
-                    context, "Nebol zadaný public key.",
-                    Toast.LENGTH_SHORT
-                ).show()
+            if(!signInViewModel.checkPublicKey() || !signInViewModel.checkPrivateKey() || !signInViewModel.checkPinKey()){
+                Toast.makeText(context, "Chybaju udaje", Toast.LENGTH_LONG).show()
+            }else if(!signInViewModel.validatePublicKey()){
+                Toast.makeText(context, "Chybny public key", Toast.LENGTH_LONG).show()
+            }else if(!signInViewModel.validatePinKey()){
+                Toast.makeText(context, "kratky pin", Toast.LENGTH_LONG).show()
+            }else{
+                signInViewModel.insertUserToDb()
+                findNavController().navigate(R.id.action_signInFragment_to_loggedInFragment)
+            }
+
         }
     }
 }
--- app/src/main/java/com/example/semester_project_crypto_wallet/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/example/semester_project_crypto_wallet/ui/signin/SignInViewModel.kt
@@ -1,22 +1,88 @@
 package com.example.semester_project_crypto_wallet.ui.signin
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.os.Build
+import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.*
 import com.example.semester_project_crypto_wallet.data.Repository
 import com.example.semester_project_crypto_wallet.data.db.entities.Credentials
+import com.example.semester_project_crypto_wallet.ui.AES
+import kotlinx.coroutines.launch
+import android.widget.Toast
+import org.stellar.sdk.Server
+import org.stellar.sdk.*
+import org.stellar.sdk.requests.ErrorResponse
 
 class SignInViewModel(private val repository: Repository) : ViewModel() {
     val publicKeyStrEditText: MutableLiveData<String> = MutableLiveData()
     val privateKeyStrEditText: MutableLiveData<String> = MutableLiveData()
     val pinKeyEditText: MutableLiveData<String> = MutableLiveData()
 
-    lateinit var findKeyCredential: LiveData<Credentials>
+//    lateinit var findKeyCredential: LiveData<Credentials>
+//
+//    fun logInAccount() : Boolean{
+//        if (publicKeyStrEditText.value =="" || publicKeyStrEditText.value == null)
+//            return false
+//        findKeyCredential = repository.findKeyCredentials(publicKeyStrEditText.value.toString())
+//
+//        return true
+//    }
 
-    fun logInAccount() : Boolean{
-        if (publicKeyStrEditText.value =="" || publicKeyStrEditText.value == null)
+    fun checkPublicKey() : Boolean{
+        return publicKeyStrEditText.value != null
+    }
+
+    fun checkPrivateKey() : Boolean{
+        return privateKeyStrEditText.value != null
+    }
+
+    fun checkPinKey() : Boolean{
+        return pinKeyEditText.value != null
+    }
+
+    fun validatePublicKey() : Boolean{
+
+        try {
+            KeyPair.fromAccountId(publicKeyStrEditText.value)
+        } catch (e: Exception) {
+            Log.i("validatePublicKey()", e.toString())
             return false
-        findKeyCredential = repository.findKeyCredentials(publicKeyStrEditText.value.toString())
+        }
+
+        val server = Server("https://horizon-testnet.stellar.org")
+
+        try {
+            server.accounts().account(publicKeyStrEditText.value);
+        } catch (e: ErrorResponse) {
+            if (e.getCode() == 404) {
+                Log.i("validatePublicKey()", e.body)
+                return false
+            }
+        }
+
         return true
     }
+
+    fun validatePinKey() : Boolean{
+        if (pinKeyEditText.value?.length == 4){
+            return true
+        }
+        return false
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun insertUserToDb(){
+        viewModelScope.launch {
+            val encrypted_secretkey =
+                pinKeyEditText.value?.let { AES.encrypt(privateKeyStrEditText.value.toString(), it) }.toString()
+
+            try {
+                repository.insertKeyPair(Credentials(publicKeyStrEditText.value.toString(),encrypted_secretkey))
+                Log.i("***", "*****REGISTRATION SUCCESSFUL******")
+            } catch (e: Exception) {
+                Log.i("EXCEPTION", e.toString())
+            }
+        }
+    }
+
 }
